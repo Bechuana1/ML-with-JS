7043798726da0d27b32b8a2b5cd1d5f5bcc0a68a
Bechuana1 <mykbechuana@gmail.com>
2023-10-12 12:18:28 +0300

    created car class and a car obj

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const canvas = document.getElementById('myCanvas');
 canvas.height = window.innerHeight;
 canvas.width = 200;
 
+const ctx = canvas.getContext("2d"); 
 
-const ctx = canvas.getcontext("2d");
-const car = new car (100,100,30,50);
-
+const car = new Car(100, 100, 30, 50); 
 
 car.draw(ctx);
